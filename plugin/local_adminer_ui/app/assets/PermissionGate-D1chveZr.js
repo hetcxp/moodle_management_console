4a8852be313298b977689a513c2f25308bbb71d9
hetcxp <hetcxp@gmail.com>
2026-08-31 22:44:03 -0300

    build(ui): update moodle plugin distribution assets and version

---
+++ plugin/local_adminer_ui/app/assets/PermissionGate-D1chveZr.js
@@ -0,0 +1,11 @@
+import{h as r,d as a,j as t}from"./index-DiGKaLmg.js";/**
+ * @license lucide-react v1.33.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],m=r("chevron-down",i);/**
+ * @license lucide-react v1.33.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M10 11v6",key:"nco0om"}],["path",{d:"M14 11v6",key:"outv1u"}],["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]],u=r("trash-2",h),c=({capability:e,children:n,fallback:o=null})=>{const{permissions:s}=a();return s?s.is_siteadmin===1?t.jsx(t.Fragment,{children:n}):e&&s[e]===1?t.jsx(t.Fragment,{children:n}):o:o};export{m as C,c as P,u as T};
--- plugin/local_adminer_ui/version.php
+++ plugin/local_adminer_ui/version.php
@@ -2,10 +2,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_adminer_ui';
-$plugin->version      = 2026082600;
+$plugin->version      = 2026083103;
 $plugin->requires     = 2024100700;
 $plugin->maturity     = MATURITY_STABLE;
-$plugin->release      = '1.0.0';
+$plugin->release      = '1.1.2';
 $plugin->dependencies = [
-    'local_adminer_api' => 2026082601,
+    'local_adminer_api' => 2026083103,
 ];
