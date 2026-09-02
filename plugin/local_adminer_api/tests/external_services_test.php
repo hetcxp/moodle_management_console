47875981a3e76cfcd40a0d0fde73c3b78c53990d
hetcxp <hetcxp@gmail.com>
2026-09-01 21:33:41 -0300

    test(backend): add unit tests for external api services and repository delegation

--- plugin/local_adminer_api/tests/external_services_test.php
+++ plugin/local_adminer_api/tests/external_services_test.php
@@ -164,4 +164,79 @@ class external_services_test extends advanced_testcase {
         $this->assertEquals('temppass@example.com', $emails[0]->to);
         $sink->close();
     }
+
+    public function test_get_course_detail_and_enrolled_progress() {
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+
+        $course = $this->getDataGenerator()->create_course(['fullname' => 'Detail Course', 'enablecompletion' => 1]);
+        $user = $this->getDataGenerator()->create_user(['firstname' => 'Student', 'lastname' => 'One']);
+
+        $this->getDataGenerator()->enrol_user($user->id, $course->id, 'student');
+
+        $res = \local_adminer_api\external\courses::get_course_detail($course->id);
+        $this->assertIsArray($res);
+        $this->assertEquals($course->id, $res['id']);
+        $this->assertNotEmpty($res['users']);
+        $this->assertEquals($user->id, $res['users'][0]['id']);
+        $this->assertEquals(0, $res['users'][0]['progress']);
+    }
+
+    public function test_user_course_action_transaction() {
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+
+        $course = $this->getDataGenerator()->create_course();
+        $user = $this->getDataGenerator()->create_user();
+
+        $enrol_res = \local_adminer_api\external\users::user_course_action('add', $user->id, [$course->id]);
+        $this->assertTrue($enrol_res['success']);
+
+        $unenrol_res = \local_adminer_api\external\users::user_course_action('remove', $user->id, [$course->id]);
+        $this->assertTrue($unenrol_res['success']);
+    }
+
+    public function test_unauthorized_user_access() {
+        $this->resetAfterTest(true);
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        $this->expectException(\required_capability_exception::class);
+        \local_adminer_api\external\dashboard::get_dashboard();
+    }
+
+    public function test_cohort_detail_and_actions() {
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+
+        $cohort = $this->getDataGenerator()->create_cohort(['name' => 'Cohort Performance Test']);
+        $user = $this->getDataGenerator()->create_user();
+        cohort_add_member($cohort->id, $user->id);
+
+        $list_res = \local_adminer_api\external\cohorts::get_cohorts(0, 10, 'name', 'ASC', 'Cohort Performance');
+        $this->assertNotEmpty($list_res['cohorts']);
+        $this->assertEquals(1, $list_res['cohorts'][0]['memberscount']);
+
+        $detail_res = \local_adminer_api\external\cohorts::get_cohort_detail($cohort->id);
+        $this->assertEquals($cohort->id, $detail_res['id']);
+        $this->assertCount(1, $detail_res['members']);
+        $this->assertEquals($user->id, $detail_res['members'][0]['id']);
+
+        $act_res = \local_adminer_api\external\cohorts::cohort_action('edit', $cohort->id, 'Updated Cohort');
+        $this->assertTrue($act_res['success']);
+    }
+
+    public function test_category_detail_progress() {
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+
+        $cat = $this->getDataGenerator()->create_category(['name' => 'Cat Detail Test']);
+        $course = $this->getDataGenerator()->create_course(['category' => $cat->id]);
+
+        $res = \local_adminer_api\external\categories::get_category_detail($cat->id);
+        $this->assertIsArray($res);
+        $this->assertEquals($cat->id, $res['id']);
+        $this->assertCount(1, $res['courses']);
+        $this->assertEquals($course->id, $res['courses'][0]['id']);
+    }
 }
