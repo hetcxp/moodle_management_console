748c403f33b0a26996dbe81007e2119a53535750
hetcxp <hetcxp@gmail.com>
2026-09-03 15:30:29 -0300

    chore(version): bump local_adminer_api plugin version

--- plugin/local_adminer_api/version.php
+++ plugin/local_adminer_api/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_adminer_api';
-$plugin->version   = 2026090101;
+$plugin->version   = 2026090301;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.4';
+$plugin->release   = '1.1.5';
